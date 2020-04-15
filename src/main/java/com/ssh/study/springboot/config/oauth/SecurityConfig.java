7002f98fb66a54bde90054e35a32157d23be2d35
dssh87 <dssh87@gmail.com>
2020-04-16 00:48:02 +0900

    스프링 부트와 aws로 혼자 구현하는 웹 서비스 5장 진행  - 202020416 스프링 시큐리티 진행 중

---
+++ src/main/java/com/ssh/study/springboot/config/oauth/SecurityConfig.java
@@ -0,0 +1,12 @@
+package com.ssh.study.springboot.config.oauth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+}
