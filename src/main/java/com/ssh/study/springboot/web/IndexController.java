7743dc3c49b5b2c8c31e4899e119e1287994c965
dssh87 <dssh87@gmail.com>
2020-05-13 01:06:10 +0900

    스프링 부트와 aws로 혼자 구현하는 웹 서비스 view page 추가  - 202020513 view 페이지 구현 에러 1 -> 상대경로에 '/'추가

--- src/main/java/com/ssh/study/springboot/web/IndexController.java
+++ src/main/java/com/ssh/study/springboot/web/IndexController.java
@@ -19,7 +19,7 @@ public class IndexController {
     @GetMapping("/")
     public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        System.out.println("index controller ... "+model);
+
         if (user != null) { // userName이라고 할 경우 윈도우의 userName을 가져오기 때문에 다른 변수명을 사용하는 것이 좋다
             model.addAttribute("userName1", user.getName());
         }
@@ -27,14 +27,21 @@ public class IndexController {
     }
 
     @GetMapping("/view")
-    public String view() {
+    public String view(Model model, @LoginUser SessionUser user) {
         System.out.println("view controller");
+        model.addAttribute("posts", postsService.findAllDesc());
+        if (user != null) { // userName이라고 할 경우 윈도우의 userName을 가져오기 때문에 다른 변수명을 사용하는 것이 좋다
+            model.addAttribute("userName1", user.getName());
+        }
         return "view";
     }
 
     @GetMapping("/posts/save")
-    public String postsSave() {
+    public String postsSave(Model model, @LoginUser SessionUser user) {
         System.out.println("save controller");
+        if (user != null) { // userName이라고 할 경우 윈도우의 userName을 가져오기 때문에 다른 변수명을 사용하는 것이 좋다
+            model.addAttribute("userName1", user.getName());
+        }
         return "posts-save";
     }
 
